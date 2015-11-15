f19f2b9428f93f657ebc7eaf798d43eb9d145a98
Herman Junge <chpdg42@gmail.com>
2015-11-14 23:22:49 -0300

    Delete ELK, change email address

--- elk/gen_crt_key.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-sudo openssl req -config /etc/ssl/openssl.cnf -x509 -days 3650 -batch -nodes -newkey rsa:2048 -keyout ./tls/private/logstash-forwarder.key -out ./tls/certs/logstash-forwarder.crt
